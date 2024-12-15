cda5984f0f41894cf0d6409d39ae73d50524c432
kaung4T <mrkaungminnkhant@gmail.com>
2024-12-15 23:03:47 +0630

    update

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,11 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.get('/note/:userName/:userId', (c) => {
+  const userName = c.req.param('userName');
+  const userId = c.req.param('userId');
+
+  return c.text(`Hello ${userName} and ${userId}`)
+})
+
 export default app
